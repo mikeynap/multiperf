0b9e0c388d67ea14efb98962f16e23bd697e6293
Michael Napolitano <mnapolit@cisco.com>
2016-06-08 16:41:10 -0400

    Fixed Zombie Procs

--- sender.c
+++ sender.c
@@ -4,7 +4,9 @@
 #include <string.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "receiver.h"
 
 /* reverse:  reverse string s in place */
@@ -39,14 +41,21 @@ void m_itoa(int n, char s[])
 
 
 int sender(int nAddr, int nStream, float bandwidth, int packet_size, int test_time, int test_inc, char *addr, int start_port, int verbose){
-    int i, j, pid;
-	int k = 1;
+    int i, j, pid, k;
 	char bw[20];
 	sprintf(bw, "%fm",bandwidth);
 	char ttime[8];
 	m_itoa(test_time, ttime);
 	char pktsize[8];
 	m_itoa(packet_size, pktsize);
+	for (k = 0; k < 8; k++){
+		if (pktsize[k] == '\0'){
+			pktsize[k] = 'B';
+			pktsize[k + 1] = '\0';
+			break;
+		}
+	}
+	k = 1;
 	if (test_inc == 0) k = nAddr;
 	int n = 1;
 	signal(SIGCHLD, SIG_IGN);
@@ -55,12 +64,10 @@ int sender(int nAddr, int nStream, float bandwidth, int packet_size, int test_ti
 		if (nStream > 1) plural_s = "s";
 		char *plural_a = "";
 		if (nAddr > 1) plural_a = "es";
-		int pids[k * nStream];
 		if (verbose == 1) printf("Test %d: Sending to %d/%d Address%s (starting at %s:%d) over %d stream%s, at %.2f mbps for %d seconds.\n", n, k, nAddr, plural_a, addr, start_port,nStream,plural_s, bandwidth, test_time);
 		for(i = 0; i < k; i++) {
 			for (j = 0; j < nStream; j++){
-			    pids[i * k + j] = fork();
-				pid = pids[i * k + j];
+				pid = fork();
 			    if(pid < 0) {
 			        printf("Error");
 			        exit(1);
@@ -77,11 +84,7 @@ int sender(int nAddr, int nStream, float bandwidth, int packet_size, int test_ti
 			}
 		}
 		n++;
-		sleep(test_time + 4);
-		for (i = k * nStream - 1; i >= 0; i--){
-			waitpid(pids[i], NULL,WNOHANG);
-		}
-		
+		sleep(test_time + 4);		
 		if (k == nAddr) break;
 		if (k == 1 && test_inc != 1) k = 0;
 		k += test_inc;
