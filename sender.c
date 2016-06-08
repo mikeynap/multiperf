0a73d1af460f24450d19da87385f8858899e49d0
Michael Napolitano <mnapolit@cisco.com>
2016-06-08 16:30:00 -0400

    ZOmbies

--- sender.c
+++ sender.c
@@ -88,7 +88,7 @@ int sender(int nAddr, int nStream, float bandwidth, int packet_size, int test_ti
 		n++;
 		sleep(test_time + 4);
 		for (i = k * nStream - 1; i >= 0; i--){
-			waitpid(pids[i], NULL,WNOHANG);
+			//waitpid(pids[i], NULL,WNOHANG);
 		}
 		
 		if (k == nAddr) break;
