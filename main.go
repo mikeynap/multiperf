80b208671af9485d7e0643769271eead3f73318a
Michael Napolitano <mnapolit@cisco.com>
2016-09-26 17:46:51 -0600

    Mcast

--- main.go
+++ main.go
@@ -13,23 +13,25 @@ func main() {
 	mode := flag.String("mode", "sender", "sender or listener")
 	flag.Parse()
 	if *mode == "sender" {
-		addr := net.ParseIP("231.0.0.1")
+		adders := make([]net.IP, 100, 100)
+		for i := 0; i < 100; i++ {
+			fmt.Println(fmt.Sprintf("231.0.0.%d", i+1))
+			fmt.Println(net.ParseIP(fmt.Sprintf("231.0.0.%d", i+1)))
+			adders[i] = net.ParseIP(fmt.Sprintf("231.0.0.%d", i+1))
+		}
 		for i := 0; i < 99; i++ {
-			addr2 := addr
-			if s, e := sender.NewSender(addr2, 9999, 1, 5, 1316); e == nil {
-				fmt.Printf("%d: %v\n", i, addr2)
-				go s.Send(addr, 9999)
-				fmt.Printf("%d: %v\n", i, addr2)
-				//addr = socket.Inc(addr)
-				fmt.Printf("%d: %v\n", i, addr2)
+			if s, e := sender.NewSender(adders[i], 9999, 1, 5, 1316); e == nil {
+				fmt.Printf("%d: %v\n", i, adders[i])
+				go s.Send(adders[i], 9999)
+				fmt.Printf("%d: %v\n", i, adders[i])
+				fmt.Printf("%d: %v\n", i, adders[i])
 			} else {
 				fmt.Println("Uh errorz")
 				fmt.Println(e)
 			}
 		}
-		addr2 := addr
-		s, _ := sender.NewSender(addr2, 9999, 1, 5, 1316)
-		s.Send(addr2, 9999)
+		s, _ := sender.NewSender(adders[99], 9999, 1, 5, 1316)
+		s.Send(adders[99], 9999)
 	} else {
 		r, e := receiver.NewListener(net.ParseIP("231.0.0.1"), 9999, 100, 5, 1316)
 		if e != nil {
